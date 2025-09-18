b560ec20f7ef3a5ca5c51f3b42e425a72098f9f6
Dirav-2712 <123054093+Dirav-2712@users.noreply.github.com>
2025-09-18 18:16:40 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,30 @@
-import * as React from "react"
+import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  const home = data.contentfulHomepage
+  const hero = getImage(home.heroImage)
+  return (
+    <main style={{ maxWidth: 900, margin: "0 auto", padding: 20 }}>
+      <h1>{home.title}</h1>
+      <p>{home.subtitle}</p>
+      {hero && <GatsbyImage image={hero} alt={home.title} />}
+      <section style={{ marginTop: 20 }}>
+        {documentToReactComponents(JSON.parse(home.intro.raw))}
+      </section>
+    </main>
+  )
 }
+
+export const query = graphql`
+  query {
+    contentfulHomepage {
+      title
+      subtitle
+      intro { raw }
+      heroImage { gatsbyImageData(layout: CONSTRAINED, width: 1200) }
+    }
+  }
+`
