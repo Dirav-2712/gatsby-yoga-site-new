a06985aebe388e9afc983090aa29fdcb260c5e8c
Dirav-2712 <123054093+Dirav-2712@users.noreply.github.com>
2025-09-18 18:17:16 +0530

    Create classes.js

---
+++ src/pages/classes.js
@@ -0,0 +1,46 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+export default function Classes({ data }) {
+  const classes = data.allContentfulYogaClass.nodes;
+  return (
+    <main style={{ maxWidth: 900, margin: "0 auto", padding: 20 }}>
+      <h1>Classes</h1>
+      {classes.map(c => {
+        const img = getImage(c.image);
+        return (
+          <article key={c.slug} style={{ borderBottom: "1px solid #eee", paddingBottom: 18, marginBottom: 18 }}>
+            <h2>{c.title}</h2>
+            <p><strong>Schedule:</strong> {c.schedule}</p>
+            {img && <GatsbyImage image={img} alt={c.title} />}
+            <div>{documentToReactComponents(JSON.parse(c.description.raw))}</div>
+            {c.videoUrl && <iframe title={c.title} src={convertToEmbed(c.videoUrl)} width="560" height="315" frameBorder="0" allowFullScreen style={{ marginTop: 12 }} />}
+          </article>
+        )
+      })}
+    </main>
+  )
+}
+
+function convertToEmbed(url) {
+  if(!url) return "";
+  const m = url.match(/(?:v=|youtu\.be\/)([A-Za-z0-9_\-]+)/);
+  return m ? `https://www.youtube.com/embed/${m[1]}` : url;
+}
+
+export const query = graphql`
+  query {
+    allContentfulYogaClass(sort: {fields: title, order: ASC}) {
+      nodes {
+        title
+        slug
+        schedule
+        videoUrl
+        image { gatsbyImageData(width: 800) }
+        description { raw }
+      }
+    }
+  }
+`
